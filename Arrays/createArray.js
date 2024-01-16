6d979605489c524d2717609cba88948a9cf715cc
manvigautam09 <mnvsgautam9@gmail.com>
2024-01-17 00:00:04 +0530

    Add basic array creation logic

---
+++ Arrays/createArray.js
@@ -0,0 +1,21 @@
+class MyArray {
+  constructor() {
+    this.length = 0;
+    this.data = {};
+  }
+
+  get(index) {
+    return this.data[index];
+  }
+
+  push(item) {
+    this.data[this.length] = item;
+    this.length++;
+  }
+  pop() {
+    const lastItem = this.data[this.length - 1];
+    delete this.data[this.length - 1];
+    this.length--;
+    return lastItem;
+  }
+}
